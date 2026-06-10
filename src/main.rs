3a19044a8d5e6b103297b9b3301ed91385249758
Christoph Blessing <chris24.blessing@gmail.com>
2026-06-10 15:58:27 +0200

    write uid/gid mappings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::fs::{create_dir, remove_dir};
+use std::fs;
 use std::os::fd::{BorrowedFd, IntoRawFd};
 use std::{thread::sleep, time::Duration};
 
 use nix::mount::{MntFlags, MsFlags, mount, umount2};
 use nix::sched::{CloneFlags, clone};
-use nix::unistd::{chdir, close, pipe, pivot_root, read, write};
+use nix::unistd::{chdir, close, getgid, getuid, pipe, pivot_root, read, write};
 
 const STACK_SIZE: usize = 1024 * 1024;
 
@@ -49,11 +49,11 @@ fn main() {
             None::<&str>,
         )
         .expect("failed to bind mount new_root");
-        create_dir(old_root).expect("failed to create old_root");
+        fs::create_dir(old_root).expect("failed to create old_root");
         chdir("/").expect("failed to change current working directory");
         pivot_root(new_root, old_root).expect("failed to pivot root");
         umount2(old_root_after_pivot, MntFlags::MNT_DETACH).expect("failed to unmount old_root");
-        remove_dir(old_root_after_pivot).expect("failed to remove old_root");
+        fs::remove_dir(old_root_after_pivot).expect("failed to remove old_root");
 
         loop {
             sleep(Duration::from_secs(1));
@@ -61,8 +61,20 @@ fn main() {
     });
 
     let pid = unsafe { clone(cb, &mut stack, flags, signal) }.expect("failed to clone process");
-
     close(read_fd).expect("failed to close read_fd in parent");
+
+    fs::write(
+        format!("/proc/{}/uid_map", pid),
+        format!("0 {} 1", getuid()),
+    )
+    .expect("failed to write to uid_map");
+    fs::write(format!("/proc/{}/setgroups", pid), "deny").expect("failed to write to setgroups");
+    fs::write(
+        format!("/proc/{}/gid_map", pid),
+        format!("0 {} 1", getgid()),
+    )
+    .expect("failed to write to gid_map");
+
     let mut buffer = vec![0u8; 1];
     let borrowed = unsafe { BorrowedFd::borrow_raw(write_fd) };
     write(borrowed, &mut buffer).expect("failed to write to pipe");
