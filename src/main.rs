9be9f997d22bd0df2d6c5e1044372c7ccc0d6b8f
Christoph Blessing <chris24.blessing@gmail.com>
2026-06-10 13:39:35 +0200

    implement root pivoting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::fs::{create_dir, remove_dir};
 use std::{thread::sleep, time::Duration};
 
-use nix::mount::{MsFlags, mount};
+use nix::mount::{MntFlags, MsFlags, mount, umount2};
 use nix::sched::{CloneFlags, clone};
+use nix::unistd::{chdir, pivot_root};
 
 const STACK_SIZE: usize = 1024 * 1024;
 
@@ -24,6 +26,23 @@ fn main() {
             None::<&str>,
         )
         .expect("failed to make mounts private");
+
+        let new_root = "/tmp/rootfs";
+        let old_root = "/tmp/rootfs/old_root";
+        let old_root_after_pivot = "/old_root";
+        mount(
+            Some(new_root),
+            new_root,
+            None::<&str>,
+            MsFlags::MS_BIND,
+            None::<&str>,
+        )
+        .expect("failed to bind mount new_root");
+        create_dir(old_root).expect("failed to create old_root");
+        chdir("/").expect("failed to change current working directory");
+        pivot_root(new_root, old_root).expect("failed to pivot root");
+        umount2(old_root_after_pivot, MntFlags::MNT_DETACH).expect("failed to unmount old_root");
+        remove_dir(old_root_after_pivot).expect("failed to remove old_root");
 
         loop {
             sleep(Duration::from_secs(1));
