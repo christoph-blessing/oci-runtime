15fc6a0e1ccfe3d5fc0e87a7678c7212c02f7f6d
Christoph Blessing <chris24.blessing@gmail.com>
2026-06-19 17:04:32 +0200

    test validate_version directly

--- src/main.rs
+++ src/main.rs
@@ -671,49 +671,13 @@ mod tests {
 
     #[test]
     fn test_invalid_version() {
-        let config = raw_config::Config {
-            oci_version: String::from("1.3.3.1234"),
-            hostname: Some(String::from("test")),
-            root: raw_config::RootConfig {
-                path: PathBuf::from("/path/to/rootfs"),
-                readonly: None,
-            },
-            mounts: None,
-            process: None,
-            linux: raw_config::LinuxConfig {
-                namespaces: vec![],
-                uid_mappings: None,
-                gid_mappings: None,
-                masked_paths: None,
-                readonly_paths: None,
-            },
-        };
-
-        let err = validate(config).unwrap_err();
-        assert!(matches!(err[0], ValidationError::InvalidVersion(_)))
+        let err = validate_version(String::from("1.2.3.4")).unwrap_err();
+        assert!(matches!(err, ValidationError::InvalidVersion(_)))
     }
 
     #[test]
     fn test_unsupported_version() {
-        let config = raw_config::Config {
-            oci_version: String::from("2.3.3"),
-            hostname: Some(String::from("test")),
-            root: raw_config::RootConfig {
-                path: PathBuf::from("/path/to/rootfs"),
-                readonly: None,
-            },
-            mounts: None,
-            process: None,
-            linux: raw_config::LinuxConfig {
-                namespaces: vec![],
-                uid_mappings: None,
-                gid_mappings: None,
-                masked_paths: None,
-                readonly_paths: None,
-            },
-        };
-
-        let err = validate(config).unwrap_err();
-        assert!(matches!(err[0], ValidationError::UnsupportedVersion))
+        let err = validate_version(String::from("2.3.3")).unwrap_err();
+        assert!(matches!(err, ValidationError::UnsupportedVersion))
     }
 }
